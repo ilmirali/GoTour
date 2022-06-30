9f56930c61680360babfd260d749310e2e1f6eaf
Ilmira <ilmirali@protonmail.com>
2022-06-30 16:46:30 +0600

    Variables declared without an explicit initial value

---
+++ 1.Basics/1.Packages.variables.functions/12.zero.go
@@ -0,0 +1,14 @@
+// Variables declared without an explicit initial value
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
