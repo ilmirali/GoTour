d6469b3ced05e758146ae722d552714e2beeb895
Ilmira <ilmirali@protonmail.com>
2022-07-18 15:53:02 +0600

    variables list in 2 lines with comma on last position

---
+++ 1.Basics/2.For/06.if-with-a-short-statement.go
@@ -0,0 +1,21 @@
+// variables list in 2 lines with comma on last position
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+}
